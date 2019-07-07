7fdb7aba0d5949b38a7f0f1e9a3d1f2672556a1d
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-07 15:40:03 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,3 +1,24 @@
-class Pirate < ActiveRecord::Base
-  belongs_to :pirate
-end
+class Pirate
+    attr_accessor :name, :weight, :height
+
+     @@all = []
+
+     def initialize(name, weight, height)
+        @name = name
+        @weight = weight
+        @height = height
+
+         @@all << self
+    end
+
+     # def initialize(args)
+    #     @name = args[:name]
+    #     @weight = args[:weight]
+    #     @height = args[:height]
+    #     @@all << self
+    # end
+
+     def self.all
+        @@all
+    end
+end 	
