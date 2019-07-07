7e0d968a71a8529c88728cba852cbbe399530dbb
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-07 15:42:27 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -10,13 +10,6 @@ class Pirate
     PIRATES << self
   end
 
-  # def initialize(args)
-  #     @name = args[:name]
-  #     @weight = args[:weight]
-  #     @height = args[:height]
-  #     @@all << self
-  # end
-
   def self.all
     PIRATES
   end
