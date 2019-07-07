770f1c2dff0483296db69df945578e19f39f29bd
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-07 15:17:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,18 +3,14 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
     # code other routes/actions here
-    get '/' do
-      erb :root
-    end
-
-    get '/new' do
-      erb :'pirates/new'
-    end
-
     get '/pirates' do
       @pirates = Pirate.all
 
-      erb :'pirates/index'
+       erb :'pirates/index'
+    end
+
+    get '/pirates/new' do
+      erb :'pirates/new'
     end
 
     get '/pirates/:id' do
@@ -24,15 +20,14 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:weight])
+      pirate = Pirate.create(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:weight])
       params[:pirate][:ships].each do |ship_data|
         ship = Ship.new(ship_data)
         ship.pirate = pirate
         ship.save
       end
 
-      redirect to "/pirates/#{pirate.id}"
+       redirect to "/pirates/#{pirate.id}"
     end
-
   end
 end
